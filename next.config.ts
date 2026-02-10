53d0698104015321f44081516bc8d871b38b1db1
Pernille-K <pernilleklarpaas@outlook.com>
2026-02-10 15:47:32 +0100

    chore(setup): make into PWA: create manifest and service worker + temporary icons
    Made the app into a PWA by creating a manifest.ts, a Service Worker, and temporary icons.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	outputFileTracingRoot: __dirname,
+	/* config options here */
 };
 
 export default nextConfig;
