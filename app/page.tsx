3d605ad37f81c4d1a772b91b30c37aa4fde7d271
Eline Fondevik <elifon@live.no>
2026-02-11 14:17:16 +0100

    chore(supabase): set up supabase client and verify db connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import ServiceWorkerRegister from "@/src/components/ServiceWorkerRegister";
+// import ServiceWorkerRegister from "@/src/components/ServiceWorkerRegister";
 
-export default function Home() {
-	return (
-		<main>
-			<h1>Test</h1>
-			<ServiceWorkerRegister></ServiceWorkerRegister>
-		</main>
-	);
+// export default function Home() {
+// 	return (
+// 		<main>
+// 			<h1>Test</h1>
+// 			<ServiceWorkerRegister></ServiceWorkerRegister>
+// 		</main>
+// 	);
+// }
+
+import { createClient } from "../src/lib/supabase/server";
+import { Suspense } from "react";
+
+async function SongData() {
+  const supabase = await createClient();
+  const { data: songs } = await supabase.from("songs").select();
+
+  return <pre>{JSON.stringify(songs, null, 2)}</pre>;
+}
+
+export default function Songs() {
+  return (
+    <Suspense fallback={<div>Loading songs...</div>}>
+      <SongData />
+    </Suspense>
+  );
 }
