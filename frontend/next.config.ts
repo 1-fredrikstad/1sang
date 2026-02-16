fc73cab1615a815da876b9fa837193d0dd1c88b9
Adele Xiao Yuan Strysse <axstryss@stud.ntnu.no>
2026-02-16 20:47:09 +0100

    pipeline: add turbopack in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	outputFileTracingRoot: __dirname,
 	/* config options here */
-};
+	experimental: {
+		turbopack: true,
+	},
+	turbopack: {
+		root: __dirname,
+	}
+} as any;
 
 export default nextConfig;
