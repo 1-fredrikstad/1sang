1ed868fa0cefd5618ec83b82a929c47939dc3785
Adele Xiao Yuan Strysse <axstryss@stud.ntnu.no>
2026-02-16 20:51:42 +0100

    pipeline: force to use webpack instead of turbopack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	outputFileTracingRoot: __dirname,
 	/* config options here */
-	experimental: {
-		turbopack: true,
-	},
-	turbopack: {
-		root: __dirname,
-	}
-} as any;
+};
 
 export default nextConfig;
