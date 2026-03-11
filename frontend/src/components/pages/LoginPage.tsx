802d3dfcb57182194f6597830b79845b27b13bc5
Adele Strysse <145263782+adelestrysse@users.noreply.github.com>
2026-03-11 23:10:37 +0100

    Fix image next warning (#53)
    * refactor: change path to login
    
    change path to login by removing it from /settings. Admins can only access the page using /admin in the URL. Update imports and paths in other files accordingly, and move some files for folder structure
    
    * test: update tests to changes made in login
    
    update tests to changes made in login/admin
    
    * fix: fix image next warning
    
    fix image next warning by giving the correct scaling of favicon.svg used in Header and LoginPage

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -14,7 +14,14 @@ export default function LoginPage() {
   return (
     <section className="grid place-items-center relative">
       <main className="flex flex-col items-center gap-4">
-        <Image src="/favicon/favicon.svg" alt="logo" width={100} height={100} />
+        <Image
+          src="/favicon/favicon.svg"
+          alt="logo"
+          width={175}
+          height={200}
+          className="h-29 w-auto"
+          priority
+        />
         <h1 className="text-3xl font-bold mb-2">Admin innlogging</h1>
         <GoogleLoginButton />
       </main>
