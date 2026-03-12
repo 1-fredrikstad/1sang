e1bca1a43fd70ae496e171d22cca19e48351bafe
Pernille-K <pernilleklarpaas@outlook.com>
2026-03-12 02:08:50 +0100

    test(colortheme): write example tests for headercolorform and themetoggle

---
+++ frontend/cypress/e2e/headercolorform.cy.ts
@@ -0,0 +1,16 @@
+describe('HeaderColorForm', () => {
+  it('renders options and allows selecting a header color', () => {
+    cy.visit('http://localhost:3000/settings');
+
+    // Wait for component to mount - use title to indicate
+    cy.contains('Fargetema', { timeout: 10000 }).should('be.visible');
+
+    // Open dropdown, see options are rendered
+    cy.get('details').first().invoke('attr', 'open', true);
+    cy.get('input[type="radio"]').should('have.length.greaterThan', 0);
+
+    // Click and verify first option
+    cy.get('input[type="radio"]').first().check({ force: true });
+    cy.get('input[type="radio"]').first().should('be.checked');
+  });
+});
