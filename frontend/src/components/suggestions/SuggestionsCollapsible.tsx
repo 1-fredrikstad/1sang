9c6fa40a1034d868d0c90bc18899d0f64f8b72cc
Pernille-K <pernilleklarpaas@outlook.com>
2026-03-25 14:00:19 +0100

    refactor: adjust animation on suggestionscollapsible
    Adjusted the animation so it looks smoother

--- frontend/src/components/suggestions/SuggestionsCollapsible.tsx
+++ frontend/src/components/suggestions/SuggestionsCollapsible.tsx
@@ -21,23 +21,25 @@ export function SuggestionsCollapsible({ suggestions }: SuggestionsCollapsiblePr
           <ChevronDownIcon className="h-5 w-5 allow-animation transition-transform duration-500 group-data-[state=open]:rotate-180" />
         </div>
       </CollapsibleTrigger>
-      <CollapsibleContent className="allow-animation min-h-10 collapsible-content data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp px-0 pb-4 mt-2 pt-1">
-        {suggestions.length === 0 ? (
-          <div className="opacity-80 overflow-hidden">Ingen ventende forslag</div>
-        ) : (
-          <ul className="space-y-2 mx-1">
-            {suggestions.map((s) => (
-              <li key={s.id}>
-                <Link
-                  href={`/admin/suggestions/${s.id}`}
-                  className="group w-full block py-4 pr-10 pl-4 rounded-sm outline-1 dark:bg-list-bg outline-[#0000001a] dark:shadow-xs dark:shadow-black hover:shadow-sm active:scale-[0.99] allow-animation transition"
-                >
-                  {s.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
+      <CollapsibleContent className="overflow-hidden collapsible-content data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp">
+        <div className="px-0 pb-4 mt-2 pt-1 allow-animation">
+          {suggestions.length === 0 ? (
+            <div className="opacity-80 overflow-hidden">Ingen ventende forslag</div>
+          ) : (
+            <ul className="space-y-2 mx-1">
+              {suggestions.map((s) => (
+                <li key={s.id}>
+                  <Link
+                    href={`/admin/suggestions/${s.id}`}
+                    className="group w-full block py-4 pr-10 pl-4 rounded-sm outline-1 dark:bg-list-bg outline-[#0000001a] dark:shadow-xs dark:shadow-black hover:shadow-sm active:scale-[0.99] allow-animation transition"
+                  >
+                    {s.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </CollapsibleContent>
     </Collapsible>
   );
