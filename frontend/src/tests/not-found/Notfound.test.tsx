181bf7089c1afc40e04645656f6fdfdebb64fca7
Pernille-K <pernilleklarpaas@outlook.com>
2026-04-14 13:34:18 +0200

    test: write test for campfire and notfoundpage

---
+++ frontend/src/tests/not-found/Notfound.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import NotFound from '@/app/not-found';
+
+describe('NotFound page', () => {
+  it('renders Campfire component on 404 page', () => {
+    const { container } = render(<NotFound />);
+
+    expect(container.querySelector('section')).toBeInTheDocument();
+  });
+});
