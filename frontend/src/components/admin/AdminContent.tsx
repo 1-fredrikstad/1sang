bed31271a438ecf6320cfaa4cff4d3a6cca97a87
Pernille-K <pernilleklarpaas@outlook.com>
2026-04-15 11:15:40 +0200

    feat(latex): place export-button in admindashboard
    Placed a button that opens the exportlatexmodal in admindashboard, and changed variant of logoutbutton for user to easier see the action is destructive

--- frontend/src/components/admin/AdminContent.tsx
+++ frontend/src/components/admin/AdminContent.tsx
@@ -1,16 +1,29 @@
 'use client';
 import LogoutButton from './LogoutButton';
 import { Spinner } from '@/components/ui/spinner';
-import { useSongSuggestions } from '@/src/hooks/useData';
+import { useSongs, useSongSuggestions } from '@/src/hooks/useData';
 import { SuggestionsCollapsible } from '../suggestions/SuggestionsCollapsible';
 import { useAuth } from '@/src/context/AuthContext';
+import ExportLatexModal from '../latex/ExportLatexModal';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 export default function AdminContent() {
   const { data: suggestions, isLoading: suggestionsLoading } = useSongSuggestions();
   const { user } = useAuth();
 
+  const [open, setOpen] = useState(false);
+
+  const { data: songs, isLoading: songsLoading } = useSongs({
+    maxAgeMins: 5,
+  });
+
   if (suggestionsLoading) {
     return <Spinner message="Laster inn admin" />;
+  }
+
+  if (songsLoading) {
+    return <Spinner message="Laster inn sanger" />;
   }
 
   if (!user) {
@@ -23,13 +36,26 @@ export default function AdminContent() {
 
   return (
     <main className="mb-5 flex flex-col justify-between">
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between mb-10">
         <div>
           <p>Logget inn som:</p>
           <b>{user.name || 'admin'}</b>
         </div>
         <LogoutButton />
       </div>
+
+      <section className="flex flex-row justify-between">
+        <p>Trykk på knappen for å eksportere sanger til LaTeX</p>
+        <Button
+          variant="secondary"
+          onClick={() => setOpen(true)}
+          className="text-md cursor-pointer"
+        >
+          Eksporter
+        </Button>
+        <ExportLatexModal open={open} onOpenChange={setOpen} songs={songs || []} />
+      </section>
+
       <article className="allow-animation mt-5">
         <SuggestionsCollapsible suggestions={suggestions || []} />
       </article>
