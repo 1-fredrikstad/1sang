54f7dfe1832f75a91c10c9e5ba35285455bb5973
Pernille-K <pernilleklarpaas@outlook.com>
2026-04-16 20:41:09 +0200

    chore: fix ci error

--- frontend/src/tests/components/latex/GenerateLatex.test.tsx
+++ frontend/src/tests/components/latex/GenerateLatex.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { generateLatex } from '@/src/components/latex/GenerateLatex';
+import { generateLatex } from '../../../components/latex/GenerateLatex';
 
 describe('GenerateLatex', () => {
   const mockSongs = [
