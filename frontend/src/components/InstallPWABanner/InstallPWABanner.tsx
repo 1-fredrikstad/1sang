d324c89c3234ad8aa322d6e0be5f960fa6685dc4
Pernille Klarpås <97983616+Pernille-K@users.noreply.github.com>
2026-04-20 13:09:52 +0200

    feat: persist close button pwabanner user session (#168)
    Added sessionstorage for close button in installpwabanner and hook so that the action persists across the session and does not annoy the user. Updated test and changed title of app in banner to correct name.

--- frontend/src/components/InstallPWABanner/InstallPWABanner.tsx
+++ frontend/src/components/InstallPWABanner/InstallPWABanner.tsx
@@ -14,31 +14,34 @@ import {
 } from '@/components/ui/alert-dialog';
 import { InstallButton } from './InstallButton';
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
 
 export default function InstallPWABanner() {
-  const { install, dismiss, showInstallButton } = usePWAInstall();
-  const [isOpen, setIsOpen] = useState(true);
+  const { install, dismiss, close, showInstallButton } = usePWAInstall();
 
   // --- Render checks ---
-  if (!showInstallButton || !isOpen) return null;
+  if (!showInstallButton) return null;
 
   return (
-    <AlertDialog open={showInstallButton} onOpenChange={setIsOpen}>
+    <AlertDialog
+      open={showInstallButton}
+      onOpenChange={(open) => {
+        if (!open) close();
+      }}
+    >
       <AlertDialogContent size="sm">
         <AlertDialogHeader>
           <div className="flex flex-row">
             <AlertDialogTitle className="leading-tight px-10">
-              Installer Sanger under Liljen som app
+              Installer 1sang som app
             </AlertDialogTitle>
             <XMarkIcon
-              onClick={() => setIsOpen(false)}
+              onClick={() => close()}
               aria-label="Lukk"
               className="text-red-500 h-7 pl-1 hover:cursor-pointer"
             />
           </div>
           <AlertDialogDescription>
-            Installer appen for bedre opplevelse og offline tilgang!
+            Installer appen for en bedre opplevelse og tilgang offline
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
