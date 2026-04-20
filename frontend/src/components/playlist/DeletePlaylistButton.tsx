b34585b61e347365093006790fc0fc378b3af910
Eline Fondevik <elifon@live.no>
2026-04-20 18:47:17 +0200

    refactor: change button component for DeletePlaylistButton

--- frontend/src/components/playlist/DeletePlaylistButton.tsx
+++ frontend/src/components/playlist/DeletePlaylistButton.tsx
@@ -16,6 +16,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
 
 type Props = {
   playlistId: string;
@@ -108,17 +109,16 @@ export function DeletePlaylistButton({
 
   return (
     <>
-      <button
+      <Button
         onClick={() => setOpen(true)}
         disabled={isDeleting}
         aria-label="Slett spilleliste"
         type="button"
-        className={`p-3 bg-danger hover:cursor-pointer hover:bg-danger-hover rounded ${
-          className ?? ''
-        }`}
+        variant={'destructive'}
+        className={`hover:cursor-pointer text-red-500 ${className ?? ''}`}
       >
-        <Image src="/trash.png" alt="" width={20} height={20} />
-      </button>
+        Slett spilleliste
+      </Button>
 
       <AlertDialog open={open} onOpenChange={setOpen}>
         <AlertDialogContent size="sm">
