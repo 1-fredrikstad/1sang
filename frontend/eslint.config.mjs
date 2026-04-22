e85ef0e251b968c50df1bc25507b352a04db9faa
Adele Xiao Yuan Strysse <axstryss@stud.ntnu.no>
2026-04-22 17:58:09 +0200

    fix: offline
    use serwist for service worker to provide offline use of app

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = defineConfig([
     'out/**',
     'build/**',
     'next-env.d.ts',
+    // Generated service worker - not to be linted:
+    'public/sw.js',
   ]),
 ]);
 
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from 'next';
+import withSerwistInit from '@serwist/next';
+
+const withSerwist = withSerwistInit({
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+  cacheOnNavigation: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
 
-  turbopack: {
-    root: __dirname,
-  },
+  // turbopack: {
+  //   root: __dirname,
+  // },
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
