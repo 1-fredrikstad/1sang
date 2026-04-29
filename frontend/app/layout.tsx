2729336a1e4417d47e02a3210e2c2ed5b42de7ca
Pernille-K <pernilleklarpaas@outlook.com>
2026-04-29 23:57:21 +0200

    chore: add vercel insights and analytics
    Installed and implemented vercel speed insights and analytics to root layout to gain insight and analytics on vercel.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import ConditionalHeader from '@/src/components/global/ConditionalHeader';
 import ConditionalNavbar from '@/src/components/global/ConditionalNavbar';
 
@@ -122,6 +125,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             </ThemeProvider>
           </TooltipProvider>
         </AuthProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
