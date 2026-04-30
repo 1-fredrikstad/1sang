e0fa83ebc4bd8d94a65238e7f695dc1e4c970085
idaustad <idaustad@hotmail.com>
2026-04-30 15:16:58 +0200

    test: start writing e2e for header (#80)

---
+++ frontend/cypress/e2e/header.cy.ts
@@ -0,0 +1,17 @@
+describe('Header', () => {
+  it('navigates to homepage when clicking logo', () => {
+    // Go to a different page first
+    cy.visit('/settings');
+
+    // Wait until the page is loaded
+    cy.contains('Fargetema', { timeout: 10000 }).should('be.visible');
+
+    // Click on the header (logo or text)
+    cy.get('header').within(() => {
+      cy.get('a[aria-label="Go to homepage"]').click();
+    });
+
+    // Verify that we are on the homepage
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+  });
+});
